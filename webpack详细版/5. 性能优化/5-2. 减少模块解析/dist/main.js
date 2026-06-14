c38c9837124bb1ee09037b397e4579f7b5c46eff
傅朝俊 <fuchaojun@szkingdom.com>
2026-06-14 10:30:23 +0800

    feat: 2.2 减少模块解析

---
+++ webpack详细版/5. 性能优化/5-2. 减少模块解析/dist/main.js
@@ -0,0 +1,40 @@
+ (function(modules) { // webpackBootstrap
+ 	// The module cache
+ 	……
+ })
+/************************************************************************/
+ ({
+
+ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _test__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./test */ "./src/test.js");
+/* harmony import */ var _test__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_test__WEBPACK_IMPORTED_MODULE_0__);
+
+console.log(_test__WEBPACK_IMPORTED_MODULE_0___default.a);
+
+
+ }),
+
+ "./src/test.js":
+/*!*********************!*\
+  !*** ./src/test.js ***!
+  \*********************/
+/*! no static exports found */
+ (function(module, exports) {
+
+import $ from "jquery"
+
+export default $;
+
+
+ })
+
+ });
+//# sourceMappingURL=main.js.map
--- webpack详细版/5. 性能优化/5-2. 减少模块解析/webpack.config.js
+++ webpack详细版/5. 性能优化/5-2. 减少模块解析/webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    mode: "development",
-    devtool: "source-map",
-    module: {
-        noParse: /test/
-    }
-}
+  mode: "development",
+  devtool: "source-map",
+  module: {
+    // noParse: /jquery/, // 打包结果还会有 jquery ，但是不会进行 抽象语法树分析、拿到 dependencies、替换依赖函数 require，直接跳过这三个步骤
+    noParse: /test/,
+  },
+};
