ce20aa8810a4bca9d65021ac6b37f487aba1ca08
傅朝俊 <fuchaojun@szkingdom.com>
2026-06-17 10:41:40 +0800

    feat: 2.5 手动分包

--- webpack详细版/5. 性能优化/5-5. 手动分包/webpack.config.js
+++ webpack详细版/5. 性能优化/5-5. 手动分包/webpack.config.js
@@ -7,26 +7,27 @@ module.exports = {
   devtool: "source-map",
   entry: {
     main: "./src/index.js",
-    other: "./src/other.js"
+    other: "./src/other.js",
   },
   output: {
-    filename: "[name].[hash:5].js"
+    filename: "[name].[hash:5].js",
   },
   plugins: [
     new CleanWebpackPlugin({
       // 要清除的文件或目录
       // 排除掉dll目录本身和它里面的文件
-      cleanOnceBeforeBuildPatterns: ["**/*", "!dll", "!dll/*"]
+      cleanOnceBeforeBuildPatterns: ["**/*", "!dll", "!dll/*"], // "!dll", "!dll/*"：不清除 dll目录本身和它里面的文件
     }),
     new HtmlWebpackPlugin({
-      template: "./public/index.html"
+      template: "./public/index.html", // 模板文件
+    }),
+    // 下面不配置好像也正常？？？
+    new webpack.DllReferencePlugin({
+      manifest: require("./dll/jquery.manifest.json"),
     }),
     new webpack.DllReferencePlugin({
-      manifest: require("./dll/jquery.manifest.json")
+      manifest: require("./dll/lodash.manifest.json"),
     }),
-    new webpack.DllReferencePlugin({
-      manifest: require("./dll/lodash.manifest.json")
-    })
-  ]
+  ],
 };
 
--- webpack详细版/5. 性能优化/5-5. 手动分包/webpack.dll.config.js
+++ webpack详细版/5. 性能优化/5-5. 手动分包/webpack.dll.config.js
@@ -9,10 +9,11 @@ module.exports = {
   },
   output: {
     filename: "dll/[name].js",
-    library: "[name]" // 每个bundle暴露的全局变量名
+    library: "[name]" // 每个bundle暴露的全局变量名，否则Uncaught ReferenceError: jquery is not defined
   },
   plugins: [
     new webpack.DllPlugin({
+      // webpack 在打包过程中需要用的，若没有配置会 Error: Cannot find module './dll/jquery.manifest.json'
       path: path.resolve(__dirname, "dll", "[name].manifest.json"),
       name: "[name]"
     })
