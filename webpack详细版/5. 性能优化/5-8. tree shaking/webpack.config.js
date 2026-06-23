386fca764c1bd49cc568d709c8357fb6cf2578d0
傅朝俊 <fuchaojun@szkingdom.com>
2026-06-23 21:50:31 +0800

    feat: 3.8 tree shaking

--- webpack详细版/5. 性能优化/5-8. tree shaking/webpack.config.js
+++ webpack详细版/5. 性能优化/5-8. tree shaking/webpack.config.js
@@ -1,10 +1,14 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 const DeepScope = require("webpack-deep-scope-plugin").default;
+
 const MiniCss = require("mini-css-extract-plugin");
+// purge
 const Purgecss = require("purgecss-webpack-plugin");
+
 const path = require("path");
 const globAll = require("glob-all");
-const srcAbs = path.resolve(__dirname, "src"); //得到src的绝对路径
+const srcAbs = path.resolve(__dirname, "src"); // 得到src的绝对路径
 const htmlPath = path.resolve(__dirname, "public/index.html");
 const paths = globAll.sync([`${srcAbs}**/*.js`, htmlPath]);
 
@@ -18,6 +22,7 @@ module.exports = {
     new DeepScope(),
     new MiniCss(),
     new Purgecss({
+      // 该插件会正则匹配指定绝对路径的文件，若匹配得到，则不会删除，否则会删除
       paths
     })
   ]
