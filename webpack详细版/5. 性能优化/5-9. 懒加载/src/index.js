2e66a80d93a435bca06d492e592cc0f6fc4f2be9
傅朝俊 <fuchaojun@szkingdom.com>
2026-06-23 22:09:32 +0800

    feat: 3.9 懒加载

--- webpack详细版/5. 性能优化/5-9. 懒加载/src/index.js
+++ webpack详细版/5. 性能优化/5-9. 懒加载/src/index.js
@@ -1,12 +1,13 @@
 const btn = document.querySelector("button");
-btn.onclick = async function() {
-  //动态加载
-  //import 是ES6的草案
-  //浏览器会使用JSOP的方式远程去读取一个js模块
-  //import()会返回一个promise   （* as obj）
-  // const { chunk } = await import(/* webpackChunkName:"lodash" */"lodash-es");
-  const { chunk } = await import("./util");
+btn.onclick = async function () {
+  // 动态加载
+  // import 是ES6的草案
+  // 浏览器会使用JSOP的方式远程去读取一个js模块
+  // import()会返回一个promise   （* as obj）
+  // const { chunk } = await import("lodash-es"); // 该 chunk 默认没有命名
+   const { chunk } = await import(/* webpackChunkName:"lodash" */"lodash-es"); // 无法树摇
+  // const { chunk } = await import("./util"); // 通过 util 按需导入（export { chunk } from "lodash-es";） 来实现树摇
   const result = chunk([3, 5, 6, 7, 87], 2);
-  console.log(result);
+  console.log(result); // [ [ 3, 5 ], [ 6, 7 ], [ 87 ] ]
 };
 
