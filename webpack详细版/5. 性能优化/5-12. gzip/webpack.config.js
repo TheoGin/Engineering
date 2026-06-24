fd29a7e617e3596f8d6a3aaa4f53b4d398853f40
傅朝俊 <fuchaojun@szkingdom.com>
2026-06-24 18:37:05 +0800

    feat: 3.12 gzip

--- webpack详细版/5. 性能优化/5-12. gzip/webpack.config.js
+++ webpack详细版/5. 性能优化/5-12. gzip/webpack.config.js
@@ -1,18 +1,19 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CmpressionWebpackPlugin = require("compression-webpack-plugin")
+const CmpressionWebpackPlugin = require("compression-webpack-plugin");
+
 module.exports = {
   mode: "production",
   optimization: {
     splitChunks: {
-      chunks: "all"
-    }
+      chunks: "all",
+    },
   },
   plugins: [
     new CleanWebpackPlugin(),
     new CmpressionWebpackPlugin({
-      test: /\.js/,
-      minRatio: 0.5
-    })
-  ]
+      test: /\.js/, // 需要压缩 匹配文件
+      minRatio: 0.5, // 打包之后文件的压缩比例 是原来文件大小的一半，才会进行压缩
+    }),
+  ],
 };
 
